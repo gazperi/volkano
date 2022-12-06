96c7947d3834b237a2c4a6e27b6d012192f54a59
Alfredo Gasperi <alfredo.gasperi@magmati.com.br>
2022-12-06 16:56:28 -0300

    Trying

---
+++ README.md
@@ -0,0 +1,3 @@
+# Volkano
+
+Ref: https://medium.com/angular-in-depth/how-to-build-a-component-library-with-angular-and-storybook-718278ab976
---
+++ projects/button/src/lib/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'lib-button',
+  template: `
+    <button [disabled]="disabled">{{text}}</button>
+  `
+})
+export class ButtonComponent {
+  @Input() text: string | undefined;
+  @Input() disabled: boolean | undefined;
+}
+
+
